b9f55f18cbe6ad4a30639513e03cfa7b7f197187
saeyeon0522 <sy01lim@naver.com>
2023-08-12 22:04:25 +0900

    Upload new files

---
+++ Binary_Num/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    static void find_Bit(int n) {
+        int loc = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                System.out.print(loc);
+                if (n > 1)
+                    System.out.print(" ");
+            }
+            n /= 2;
+            loc++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        int n;
+        for (int i = 0; i < t; i++)
+        {
+            n = scanner.nextInt();
+            find_Bit(n);
+            if (i != t - 1) System.out.print(" ");
+        }
+        scanner.close();
+    }
+}
