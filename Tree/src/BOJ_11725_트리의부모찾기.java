16d7a0ebe76b661e4eaaf41917b7a7efa6efbcdb
saeyeonn <sy01lim@naver.com>
2024-03-16 03:48:09 +0900

    Add: Tree

---
+++ Tree/src/BOJ_11725_트리의부모찾기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11725_트리의부모찾기 {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+
+    }
+}
