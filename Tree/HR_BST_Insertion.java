3d203f396a35d7e97f14b9d581dbddd1126cac77
saeyeonn <sy01lim@naver.com>
2024-03-20 14:04:58 +0900

    Graph

---
+++ Tree/HR_BST_Insertion.java
@@ -0,0 +1,30 @@
+public class HR_BST_Insertion {
+    static Node Insert(Node root, int value){
+        Node newNode = new Node();
+        newNode.data = value;
+        newNode.left = null;
+        newNode.right = null;
+
+        if (root == null) {
+            return newNode;
+        }
+
+        addNode(root, newNode);
+        return root;
+    }
+
+    static void addNode(Node root, Node newNode) {
+        if (newNode.data < root.data) {
+            if (root.left == null) {
+                root.left = newNode;
+            } else {
+                addNode(root.left, newNode);
+            }
+        } else {
+            if (root.right == null) {
+                root.right = newNode;
+            } else {
+                addNode(root.right, newNode);
+            } // else
+        } // else
+}
---
+++ Tree/HR_IsThisBST.java
@@ -0,0 +1,15 @@
+public class HR_IsThisBST {
+    boolean checkBST(Node root) {
+        return checkBst(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    boolean checkBst(Node root, int min, int max){
+        if (root == null) return true;
+
+        return
+                min < root.data &&
+                        root.data < max &&
+                        checkBst(root.left, min, root.data) &&
+                        checkBst(root.right, root.data, max);
+    }
+}
